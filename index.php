38c23cfce095abbd9d2dfb62d4c6770da27ea0c0
Kristian Džoić <153235817+kiko4902@users.noreply.github.com>
2024-12-02 17:21:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                     padding: 0.25em 2em 0.25em 0em;
                     border: 0 none;
                 }
-            </style>
+            </style>a
     </head>
     <body>
         <h1>Register here!</h1>
@@ -57,60 +57,60 @@
     <?php
 // DB connection info
 // TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved earlier from the Azure Portal. $host = "value of Data Source";
-$host = "lv3app-server.mysql.database.azure.com";
-$user = "uridqrntch";
-$pwd = 'QrNNoKjmA$DRxu81';
-$db = "lv3App-database";
+        $host = "lv3app-server.mysql.database.azure.com";
+        $user = "uridqrntch";
+        $pwd = 'QrNNoKjmA$DRxu81';
+        $db = "lv3App-database";
 // Connect to database.
-$conn = mysqli_connect($host, $user, $pwd, $db);
-if (mysqli_connect_errno())
-{
-echo "<h3>Failed to connect to MySQL:</h3> " . mysqli_connect_error();
-}
- else
-{
-// echo '<h3>Connected successfully</h3>';
-}
-// Insert registration info
-if (!empty($_POST))
-{
-$name = $_POST['name'];
-$email = $_POST['email'];
-$date = date("Y-m-d");
+        $conn = mysqli_connect($host, $user, $pwd, $db);
+        if (mysqli_connect_errno())
+        {
+            echo "<h3>Failed to connect to MySQL:</h3> " . mysqli_connect_error();
+        }
+        else
+        {
+        echo '<h3>Connected successfully</h3>';
+        }
+        // Insert registration info
+        if (!empty($_POST))
+        {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $date = date("Y-m-d");
 // Insert data
-$sql_insert = "INSERT INTO registration_tbl (name, email, date)
-VALUES ('$name','$email','$date')";
-if ($conn->query($sql_insert) === TRUE)
-{
-echo "<h3>Your're registered!</h3>";
+            $sql_insert = "INSERT INTO registration_tbl (name, email, date)
+            VALUES ('$name','$email','$date')";
+            if ($conn->query($sql_insert) === TRUE)
+            {
+                echo "<h3>Your're registered!</h3>";
 // // Retrieve data
-$sql_select = "SELECT * FROM registration_tbl";
-$registrants = $conn->query($sql_select);
-if ($registrants->num_rows > 0)
-{
-echo "<h2>People who are registered:</h2>";
-echo "<table>";
-echo "<tr><th>Name</th>";
-echo "<th>Email</th>";
-echo "<th>Date</th></tr>";
-while ($registrant = $registrants->fetch_assoc())
-{
-echo "<tr><td>" . $registrant['name'] . "</td>";
-echo "<td>" . $registrant['email'] . "</td>";
-echo "<td>" . $registrant['date'] . "</td></tr>";
-}
-echo "</table>";
-}
- else
-{
-echo "<h3>No one is currently registered.</h3>";
-}
-}
- else
-{
-    echo "Insert Failed";
-}
-}
-?>
+                $sql_select = "SELECT * FROM registration_tbl";
+                $registrants = $conn->query($sql_select);
+                if ($registrants->num_rows > 0)
+                {
+                    echo "<h2>People who are registered:</h2>";
+                    echo "<table>";
+                    echo "<tr><th>Name</th>";
+                    echo "<th>Email</th>";
+                    echo "<th>Date</th></tr>";
+                    while ($registrant = $registrants->fetch_assoc())
+                    {
+                        echo "<tr><td>" . $registrant['name'] . "</td>";
+                        echo "<td>" . $registrant['email'] . "</td>";
+                        echo "<td>" . $registrant['date'] . "</td></tr>";
+                    }
+                    echo "</table>";
+                }
+                else
+                {
+                    echo "<h3>No one is currently registered.</h3>";
+                }
+            }
+            else
+            {
+                echo "Insert Failed";
+            }
+        }
+    ?>
 </body>
 </html>
