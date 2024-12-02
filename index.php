665518c6574728f9321b8737865ca4416f3cbf89
Kristian Džoić <153235817+kiko4902@users.noreply.github.com>
2024-12-02 17:50:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -71,27 +71,6 @@
         {
         echo '<h3>Connected successfully</h3>';
         }
-         $sql = 'CREATE Database ruapdb';
-         if ($conn->query($sql) === TRUE) {
-         echo "Database created successfully";
-         } else {
-         echo "Error creating database: " . $conn->error;
-         }
-         mysqli_select_db($conn,$db);
-         $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
-         AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
-         VARCHAR(30), date DATE);";
-         if ($conn->query($sql_create) === TRUE) {
-         echo "Table registration_tbl created successfully";
-         } else {
-         echo "Error creating table: " . $conn->error;
-         }
-        // Insert registration info
-        if (!empty($_POST))
-        {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $date = date("Y-m-d");
 // Insert data
             $sql_insert = "INSERT INTO registration_tbl (name, email, date) VALUES ('$name','$email','$date')";
             if ($conn->query($sql_insert) === TRUE)
